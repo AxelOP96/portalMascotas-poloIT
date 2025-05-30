a140f0628bf649e1c5488356a25a2ead4855df33
Brian <brianbasabee@gmail.com>
2025-05-30 14:41:08 -0300

    Feature:Agregado del Navbar + Imagenes + Estilos

---
+++ proyecto-poloit/src/app/components/HeaderCurve.tsx
@@ -0,0 +1,17 @@
+export default function HeaderCurve() {
+    return (
+        <svg
+            viewBox="0 0 1440 150"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            className="header-curve"
+        >
+            <path
+                d="M0,64L60,69.3C120,75,240,85,360,85.3C480,85,600,75,720,74.7C840,75,960,85,1080,90.7C1200,96,1320,96,1380,96L1440,96L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
+                fill="#3fc5ad"
+                style={{ filter: "drop-shadow(0 4px 4px rgba(255, 191, 163, 0.63))" }}
+            />
+
+        </svg>
+    );
+}
--- proyecto-poloit/src/app/page.tsx
+++ proyecto-poloit/src/app/page.tsx
@@ -1,33 +1,30 @@
 "use client";
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-white font-sans relative overflow-hidden">
-      <header className="bg-[#3fc5ad] w-full py-4 px-8 flex justify-between items-center text-white">
-        <Image src="/mihogar-logo-mini.png" alt="MiHogar" width={120} height={40} />
-        <a href="#peluditos" className="font-bold text-sm uppercase tracking-wide">Ver Peluditos</a>
-      </header>
+      <Navbar />
 
       <main className="flex-grow flex flex-col justify-center items-center relative">
-        
         <Image
           src="/mihogar-logo.png"
           alt="Logo MiHogar"
-          width={400}
+          width={800}
           height={200}
           className="z-10"
         />
-        
-        <div className="absolute top-[80px] left-[10px] opacity-30">
-          <Image src="/huellas-left.png" alt="huellas" width={60} height={60} />
+
+        <div className="absolute top-[80px] left-[10px] opacity-180">
+        <Image src="/huellas-left.png" alt="Huellas izquierda" width={160} height={60} />
         </div>
-        <div className="absolute bottom-[80px] right-[10px] opacity-30">
-          <Image src="/huellas-right.png" alt="huellas" width={60} height={60} />
+        <div className="absolute bottom-[80px] right-[10px] opacity-180">
+        <Image src="/huellas-right.png" alt="Huellas izquierda" width={160} height={60} />
+
         </div>
       </main>
 
-      
       <footer className="w-full h-[100px] bg-[#3fc5ad] rounded-t-[100px]" />
     </div>
   );
